014ff3ddecd09c0f571d7b46cef66fa075ba21bb
Cheikh Mbacke <mbackecheikhouna7@gmail.com>
2023-08-04 13:39:30 +0200

    succes msg

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,13 +10,13 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
-const modalBtnClose = document.querySelector('.close'); // on récupère le bouton close de la modal
+const modalBtnClose = document.querySelector('.close'); // ISSUE#1: on récupère le bouton close de la modal
 const formData = document.querySelectorAll('.formData');
 const modalBody = document.querySelector('.modal-body'); // on récupère le modal-body
 
 // Formulaire
 const form = document.getElementById('reserve');
-const validationMsg = document.querySelector('.validationMessage');
+const validateForm = document.querySelector('validateForm');
 
 // Récupérer les champs du formulaire
 const firstName = document.getElementById('first');
@@ -25,10 +25,8 @@ const email = document.getElementById('email');
 const birthdate = document.getElementById('birthdate');
 const quantity = document.getElementById('quantity');
 const city = document.querySelectorAll('[name="location"]');
-//const location = document.getElementsByName('location'); //test
-//console.log(city,"SELECTTTTCITYYYYYYYYYYY"); //test
 const cgu = document.getElementById('checkbox1');
-const validMessage = document.getElementById('validMessage');
+const validateMessage = document.getElementById('validateMessage');
 const btnSubmit = document.getElementById('btnSubmit');
 const btnValid = document.getElementById('btnValid');
 
@@ -47,25 +45,29 @@ function closeModal() {
 
 // Listener sur les champs
 firstName.addEventListener('change', function () {
+      validateFirstName(this);
+    
+      
       // console.log(firstName.value, 'firstname');
 });
 
 lastName.addEventListener('change', function () {
+      validateLastName(this);
       // console.log(lastName.value, 'lastname');
 });
 
 email.addEventListener('change', function () {
+      validateEmail(this);
       // console.log(email.value, 'email');
 });
 
-// test
 birthdate.addEventListener('change', function () {
-      validateBirthdate();
+      validateBirthdate(this);
       // console.log(birthdate.value, 'birthdate')
 });
 
 quantity.addEventListener('change', function () {
-      validateQuantity();
+      validateQuantity(this);
       // console.log(quantity.value, 'quantity')
 });
 
@@ -74,15 +76,25 @@ quantity.addEventListener('change', function () {
 /* fonction validation du formulaire */
 function validate() {
 
-      
-      validateFirstName(); 
-      validateLastName(); 
-      validateEmail(); 
-      validateBirthdate(); 
-      validateQuantity();
-      validateCity();
-      validateCgu();
-      return false; //A remettre sur true
+      if (
+            validateFirstName() && 
+            validateLastName() &&
+            validateEmail() &&
+            validateBirthdate() && 
+            validateQuantity() &&
+            validateCity() &&
+            validateCgu()
+            // prochain event !!!
+            
+            ) {
+            
+            console.log('envoi du formulaire');
+            console.log('je rentre dans le thanks')
+            openModalThanking();
+      } else {
+            return false;
+      }
+      return true; //A remettre sur true
 }
 
 // Close modal function: ajout d'une fonction et du listener puis un display none
@@ -99,9 +111,10 @@ modalBtnClose.addEventListener('click', function () {
  */
 
 function validateFirstName() {
+      // A vérifier pour empecher la saisie de chiffre
       const regexFirstName = /^[a-zA-z -ê/ë/é]{2,30}$/; /*min 2 caractères*/
       const parent = document.getElementById('first').parentNode;
-            if (firstName.value == '' || !regexFirstName.test(firstName.value)) {
+            if (firstName.value.trim() == '' || !regexFirstName.test(firstName.value)) {
                   firstName.focus();
                   parent.setAttribute(
                         'data-error',
@@ -119,7 +132,7 @@ function validateLastName() {
       // a vérifier
       const regexLastName = /^[a-zA-z -ê/ë/é]{2,30}$/; /*min 2 caracteres*/
       const parent = document.getElementById('last').parentNode;
-            if (lastName.value == '' || !regexLastName.test(lastName.value)) {
+            if (lastName.value.trim() == '' || !regexLastName.test(lastName.value)) {
                   lastName.focus();
                   parent.setAttribute(
                         'data-error',
@@ -136,7 +149,7 @@ function validateLastName() {
 function validateEmail() {
       const regexEmail = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z]{2,}\.[a-zA-Z]{2,}$/;
       const parent = document.getElementById('email').parentNode;
-            if (email.value == '' || !regexEmail.test(email.value)) {
+            if (email.value.trim() == '' || !regexEmail.test(email.value)) {
                   email.focus();
                   parent.setAttribute(
                         'data-error',
@@ -153,7 +166,6 @@ function validateEmail() {
 function validateBirthdate() {
       const parent = document.getElementById('birthdate').parentNode;
             if (!birthdate.value) {
-                  
                   birthdate.focus();
                   parent.setAttribute(
                         'data-error',
@@ -184,40 +196,46 @@ function validateQuantity() {
 
 // ================ CHOIX DE VILLE ==================
 function validateCity() {
-
       if (document.querySelector('input[name="location"]:checked') == null) {
             document.querySelector('input[name="location"]').parentElement.setAttribute(
                   'data-error',
                   'Veuillez sélectionner une ville !'
             );
-            console.log(document.querySelector('input[name="location"]'),'ELEMENT');
+            // console.log(document.querySelector('input[name="location"]'),'ELEMENT');
             document.querySelector('input[name="location"]').parentElement.setAttribute('data-error-visible', 'true');
             return false;
       }
       document.querySelector('input[name="location"]').parentElement.setAttribute('data-error-visible', 'false');
       return true;
 }
-
-
   
 // TEST CGU==================
 function validateCgu() {
       const parent = document.getElementById('checkbox1').parentNode;
-      // console.log(parent, 'parent-CGU');
-      // console.log(cgu.value);
-      if (!cgu.checked) {
-            
-            cgu.focus();
-            parent.setAttribute(
-                  'data-error',
-                  'Veuillez accepter les conditions générales d\'utilisation !'
-            );
-            parent.setAttribute('data-error-visible', 'true');
-            return false;
-      }
-      parent.setAttribute('data-error-visible', 'false');
-      return true;
+            if (!cgu.checked) {
+                  cgu.focus();
+                  parent.setAttribute(
+                        'data-error',
+                        'Veuillez accepter les conditions générales d\'utilisation !'
+                  );
+                  parent.setAttribute('data-error-visible', 'true');
+                  return false;
+            }
+            parent.setAttribute('data-error-visible', 'false');
+            return true;
 }
+
+function openModalThanking(e) {
+      document.forms["reserve"].classList.add('aria-hidden');
+      document.querySelector(".confirm").classList.remove("aria-hidden");
+      document.querySelector(".confirm").classList.add("aria-succes");
+
+}
+
+// BTN SUBMIT======
+// btnValid.addEventListener('click', function() {
+//       window.location.reload();
+// });
 
 // TEST=======
 console.log(document.forms["reserve"]);
